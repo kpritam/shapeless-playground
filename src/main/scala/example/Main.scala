603403518879d8f672c6d5eaaf021756c89f273d
Pritam Kadam <kpritam@thoughtworks.com>
2019-06-05 19:26:13 +0530

    Small change in models

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -4,10 +4,10 @@ import shapeless.{HList, ::, HNil}
 object Main extends App {
 
   case class Person(fname: String, lname: String, age: Int)
-  case class Employee(fname: String, lname: String, id: Int)
+  case class Employee(name: String, id: Int)
 
   val personCsv = CsvEncoder[Person].encode(Person("Pritam", "Kadam", 29))
-  val employeeCsv = CsvEncoder[Employee].encode(Employee("Jack", "Bauer", 1827))
+  val employeeCsv = CsvEncoder[Employee].encode(Employee("Jack", 1827))
 
   println("=" * 15)
   println(personCsv.mkString(","))
